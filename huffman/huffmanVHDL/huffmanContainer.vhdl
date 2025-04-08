1d82e2b1424ed941ee8415f5965b1405efee17ce
Juopperi <albin.juopperi@gmail.com>
2025-04-08 18:09:35 +0200

    Huffman blocks and preliminary container

--- huffman/huffmanVHDL/huffmanContainer.vhdl
+++ huffman/huffmanVHDL/huffmanContainer.vhdl
@@ -13,32 +13,81 @@ end entity huff_container;
 architecture arch of huff_container is 
     component huffman_AC_Y is
         port(   
+            clk : in std_logic;
             input_integer : in std_logic_vector(7 downto 0);
             zeros : in integer;
-            output_integer: out std_logic_vector(15 downto 0);
-            length : out integer;
-            ready_next : out std_logic
+            output_bit: out std_logic;
+            done : out std_logic
         );
     end component huffman_AC_Y;
         
-        type t_State is (count_zero,load,store);
-        signal State : t_State;
+    component huffman_AC_CbCr is
+        port(   
+            clk : in std_logic;
+            input_integer : in std_logic_vector(7 downto 0);
+            zeros : in integer;
+            output_bit: out std_logic;
+            done : out std_logic
+        );
+    end component huffman_AC_CbCr;
+    
+   component huffman_DC_Y is
+        port(   
+            clk : in std_logic;
+            input_integer : in std_logic_vector(7 downto 0);
+            output_bit: out std_logic;
+            done : out std_logic
+        );
+    end component huffman_DC_Y;
+        
+    component huffman_DC_CbCr is
+        port(   
+            clk : in std_logic;
+            input_integer : in std_logic_vector(7 downto 0);
+            output_bit: out std_logic;
+            done : out std_logic
+        );
+    end component huffman_DC_CbCr;
 
         signal input_integer : std_logic_vector(7 downto 0) := (others => '0');
-        signal output_integer : std_logic_vector(15 downto 0);
-        signal ready_next : std_logic;
-        signal length : integer := 0; 
+        signal output_bit : std_logic;
+        signal done : std_logic;
         signal zeros : integer := 0;
 
     begin
         
-        test: component huffman_AC_Y
+        AC_Y: component huffman_AC_Y
             port map(
+                clk => clk,
                 input_integer => input_integer,
-                zeros => zeros,
-                output_integer => output_integer,
-                length => length,
-                ready_next => ready_next
+                zeros => zeros,               
+                output_bit => output_bit,
+                done => done
+            );
+        
+        AC_CbCr: component huffman_AC_CbCr
+            port map(
+                clk => clk,
+                input_integer => input_integer,
+                zeros => zeros,               
+                output_bit => output_bit,
+                done => done
+            );
+            
+        DC_Y: component huffman_DC_Y
+            port map(
+                clk => clk,
+                input_integer => input_integer,             
+                output_bit => output_bit,
+                done => done
+            );
+            
+        DC_CbCr: component huffman_DC_CbCr
+            port map(
+                clk => clk,
+                input_integer => input_integer,             
+                output_bit => output_bit,
+                done => done
             );
 
         proc : process(clk)
@@ -47,36 +96,7 @@ architecture arch of huff_container is
         variable min : natural range 0 to 512 := 0;
         begin
 		if rising_edge(clk) then
-            if reset = '1' then
-                index := 0;
-                max := 7;
-                min := 0;
-            else 
-                case state is 
-                    when count_zero =>
-                        if test1(max downto min) = B"0000_0000" then
-                            max := max + 8;
-                            min := min + 8; 
-                            zeros <= zeros + 1;
-                        else 
-                            state <= load;
-                        end if;
-                    when load =>
-                        input_integer <= test1(max downto min);
-                        state <= store;
-                    when store =>
-                        for k in 0 to length-1 loop
-                            test2(index) <= output_integer(k);
-                            index := index + 1;
-                        end loop;
-                        zeros <= 0;
-                        max := max + 8;
-                        min := min + 8;
-                        state <= count_zero; 
-                end case;
-                
-            end if;
-          
+           min := 0;          
    		end if;
         end process proc;
 
