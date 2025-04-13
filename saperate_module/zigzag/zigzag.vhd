823de3edc2255bbf750883607123e7ff3855294f
wenruoxu <wuchen1314134@163.com>
2025-04-13 16:56:48 +0200

    new zigzag

--- saperate_module/zigzag/zigzag.vhd
+++ saperate_module/zigzag/zigzag.vhd
@@ -1,23 +1,19 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use work.FixedPoint_Types.ALL;
 
 entity zigzag_reorder is
     port (
         clk : in std_logic;
-        input_matrix : in  fixed_array;
-        zigzag_out   : out fixed_array   
+        input_matrix : in  std_logic_vector(2047 downto 0); -- 64 x 32-bit flattened input
+        zigzag_out   : out std_logic_vector(2047 downto 0)  -- 64 x 32-bit flattened output
     );
 end entity;
 
 architecture rtl of zigzag_reorder is
 
-    -- Internal signals for the input and output arrays
-    signal input_arr  : fixed_array;
-    signal output_arr : fixed_array;
-
-    -- Zigzag order (8x8)
+    -- Zigzag order mapping for 8x8 matrix
+    type integer_array is array (0 to 63) of integer;
     constant zigzag_order : integer_array := (
          0,  1,  5,  6, 14, 15, 27, 28,
          2,  4,  7, 13, 16, 26, 29, 42,
@@ -29,22 +25,20 @@ architecture rtl of zigzag_reorder is
         35, 36, 48, 49, 57, 58, 62, 63
     );
 
+    signal internal_out : std_logic_vector(2047 downto 0);
+
 begin
 
-    input_arr <= input_matrix;
-
-    -- Perform zigzag reorder
     process(clk)
     begin
-      if rising_edge(clk) then
-        for i in 0 to 63 loop
-            output_arr(i) <= input_arr(zigzag_order(i));
-        end loop;
-      end if;
+        if rising_edge(clk) then
+            for i in 0 to 63 loop
+                internal_out(32*i + 31 downto 32*i) <=
+                    input_matrix(32*zigzag_order(i) + 31 downto 32*zigzag_order(i));
+            end loop;
+        end if;
     end process;
 
-    zigzag_out <= output_arr;
-
+    zigzag_out <= internal_out;
 
 end architecture;
-
