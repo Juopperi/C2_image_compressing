95553e0e689e3d0eb6130271cbcc2aa40e68b7d1
Juopperi <albin.juopperi@gmail.com>
2025-04-27 15:47:36 +0200

    Update conversion_no_dsp.vhdl

--- YCbCr_conversion/conversion_no_dsp.vhdl
+++ YCbCr_conversion/conversion_no_dsp.vhdl
@@ -23,50 +23,45 @@ end conversion;
 
 architecture conversion_arch of conversion is
     
-    signal R : std_logic_vector(15 downto 0);
-    signal G : std_logic_vector(15 downto 0);
-    signal B : std_logic_vector(15 downto 0);
+
     constant ZERO : std_logic_vector(7 downto 0) := (others => '0');
+    signal R : signed(15 downto 0);
+    signal G : signed(15 downto 0);
+    signal B : signed(15 downto 0);
 
 begin
 
     proc : process(clk)
         variable index : integer := 0;
-        variable temp_Y_R : std_logic_vector(15 downto 0) := (others => '0');
-        variable temp_Y_G : std_logic_vector(15 downto 0) := (others => '0');
-        variable temp_Y_B : std_logic_vector(15 downto 0) := (others => '0');
-
-        variable temp_Cb_R : std_logic_vector(15 downto 0) := (others => '0');
-        variable temp_Cb_G : std_logic_vector(15 downto 0) := (others => '0');
-        variable temp_Cb_B : std_logic_vector(15 downto 0) := (others => '0');
-
-        variable temp_Cr_R : std_logic_vector(15 downto 0) := (others => '0');
-        variable temp_Cr_G : std_logic_vector(15 downto 0) := (others => '0');
-        variable temp_Cr_B : std_logic_vector(15 downto 0) := (others => '0');
+        variable temp_Y : signed(15 downto 0) := (others => '0');
+        variable temp_Cb: signed(15 downto 0) := (others => '0');
+        variable temp_Cr: signed(15 downto 0) := (others => '0');
+ 
     begin
-        if rising_edge(clk) then 
+        if rising_edge(clk) then
             if index = 0 then
-                R <= ZEROS & input_R;
-                G <= ZEROS & input_G;
-                B <= ZEROS & input_B;
+                done <= '0';
+                R <= signed(input_R & ZERO);
+                G <= signed(input_G & ZERO);
+                B <= signed(input_B & ZERO);
             elsif index = 1 then
-                temp_Y_R := shift_left(R,6) + shift_left(R,3) + shift_left(R,2);
-                temp_Y_G := shift_left(G,7) + shift_left(G,4) + shift_left(G,2) + shift_left(G,1);
-                temp_Y_B := shift_left(G,4) + shift_left(B,3) + shift_left(B,2) + shift_left(B,0); 
+                temp_Y := (R srl 9) + (R srl 6) + (R srl 5) + (R srl 2); --0.299
+                temp_Cb := B srl 1; -- 0.5
+                 temp_Cr := R srl 1; -- 0.5                                 
             elsif index = 2 then
-                temp_Cb_R := shift_left(R,5) + shift_left(R,3) + shift_left(R,1) + shift_left(R,0); 
-                temp_Cb_G := shift_left(G,6) + shift_left(R,4) + shift_left(R,2) + shift_left(R,0); 
-                temp_Cb_B := shift_left(B,7);
+                temp_Y := temp_Y + (G srl 7) + (G srl 6) + (G srl 4) + (G srl 1); --0.587
+               temp_Cb := temp_Cb - (G srl 10) - (G srl 9) - (G srl 6) - (G srl 4) - (G srl 2); --0.3313
+                temp_Cr := temp_Cr - (G srl 7) - (G srl 5) - (G srl 3) - (G srl 2); -- 0.4187
             elsif index = 3 then
-                temp_Cr_R := shift_left(R,7);
-                temp_Cr_G := shift_left(G,6) + shift_left(G,5) + shift_left(G,3) + shift_left(G,1) + shift_left(G,0)
-                temp_Cr_B := shift_left(B,4) + shift_left(B,2) + shift_left(B,0);
+                temp_Y := temp_Y + (B srl 13) + (B srl 11) + (B srl 8) + (B srl 6) + (B srl 5)+ (B srl 4); --0.144
+                 temp_Cb := temp_Cb  -(R srl 13) -(R srl 11)-(R srl 8) -(R srl 7) - (R srl 5) - (R srl 3) ;    -- 0.1687        
+                temp_Cr := temp_Cr - (B srl 10) - (B srl 9) - (B srl 6) - (B srl 4); --0.0813
             elsif index = 4 then
-                output_Y <= temp_Y_R + temp_Y_G + temp_Y_B;
-                output_Cb <= temp_Cb_R + temp_Cb_G + temp_Cb_B;
-                output_Cr <= temp_Cr_R + temp_Cr_G + temp_Cr_B;
+                output_Y <= std_logic_vector(temp_Y) -"1000000000000000"; --128*2**8
+                output_Cb <= std_logic_vector(temp_Cb);
+                output_Cr <= std_logic_vector(temp_Cr);
             elsif index = 5 then 
-                done <= '1'
+                done <= '1';
             end if;
             index := index + 1;
             if index = 6 then
