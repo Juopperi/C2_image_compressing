42b6496c29ed16f6d6a163bf987c342bca3fce00
19elio <evm2412@gmail.com>
2025-05-14 09:45:38 +0200

    Created an alternative wrapper which incorporates the file-writing module

---
+++ connection/rtl/new/fwrit_main.vhd
@@ -0,0 +1,222 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_ARITH.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity fwrit_main is
+    Port ( height, width: in std_logic_vector (15 downto 0);
+           clk, rst, in_bit, start, datavalid, done: in std_logic;
+           dataready: out std_logic; --signals Huffman block that we are ready to receive data
+           axi_valid: out std_logic;
+           axi_ready: in std_logic;
+           axi_data: out std_logic_vector(31 downto 0);
+           axi_keep: out std_logic_vector (3 downto 0));
+end fwrit_main;
+
+architecture Behavioral of fwrit_main is
+    
+    type image_info is array (0 to 624) of std_logic_vector(7 downto 0);
+    constant img_info: image_info := (
+        x"FF", x"D8",--SOI
+        x"FF", x"E0", x"00", x"10",--APP0
+        x"4A", x"46", x"49", x"46", x"00",
+        x"01", x"01", x"00", x"00", x"01", x"00", x"01", x"00", x"00",
+        x"FF", x"DB", x"00", x"43", x"00",--Quant Y
+            x"10", x"0B", x"0C", x"0E", x"0C", x"0A", x"10", x"0E",
+            x"0D", x"0E", x"12", x"11", x"10", x"13", x"18", x"28",
+            x"1A", x"18", x"16", x"16", x"18", x"31", x"23", x"25",
+            x"1D", x"28", x"3A", x"33", x"3D", x"3C", x"39", x"33",
+            x"38", x"37", x"40", x"48", x"5C", x"4E", x"40", x"44",
+            x"57", x"45", x"37", x"38", x"50", x"6D", x"51", x"57",
+            x"5F", x"62", x"67", x"68", x"67", x"3E", x"4D", x"71",
+            x"79", x"70", x"64", x"78", x"5C", x"65", x"67", x"63",
+        x"FF", x"DB", x"00", x"43", x"01",--Quant C
+            x"11", x"12", x"18", x"2F", x"63", x"63", x"63", x"63",
+            x"12", x"15", x"1A", x"42", x"63", x"63", x"63", x"63",
+            x"18", x"1A", x"38", x"63", x"63", x"63", x"63", x"63",
+            x"2F", x"42", x"63", x"63", x"63", x"63", x"63", x"63",
+            x"63", x"63", x"63", x"63", x"63", x"63", x"63", x"63",
+            x"63", x"63", x"63", x"63", x"63", x"63", x"63", x"63",
+            x"63", x"63", x"63", x"63", x"63", x"63", x"63", x"63",
+            x"63", x"63", x"63", x"63", x"63", x"63", x"63", x"63",
+        x"FF", x"C0", x"00", x"11", x"08",--SOF
+        x"00", x"00", x"00", x"00",--here are height and width
+        x"03", x"01", x"11", x"00", x"02", x"11", x"01", x"03", x"11", x"01",
+        x"FF", x"C4", x"00", x"1F", x"00",--DHT Y DC
+            x"00", x"01", x"05", x"11", x"11", x"11", x"11", x"11", x"11", x"00", x"00", x"00", x"00", x"00", x"00", x"00",
+            x"00", x"01", x"02", x"03", x"04", x"05", x"06", x"07", x"08", x"09", x"0A", x"0B",
+        x"FF", x"C4", x"00", x"B5", x"10",--DHT Y AC
+            x"00", x"02", x"01", x"03", x"03", x"02", x"04", x"03", x"05", x"05", x"04", x"04", x"00", x"00", x"01", x"7D",
+            x"01", x"02", x"03", x"00", x"04", x"11", x"05", x"12",
+            x"21", x"31", x"41", x"06", x"13", x"51", x"61", x"07",
+            x"22", x"71", x"14", x"32", x"81", x"91", x"A1", x"08",
+            x"23", x"42", x"B1", x"C1", x"15", x"52", x"D1", x"F0",
+            x"24", x"33", x"62", x"72", x"82", x"09", x"0A", x"16",
+            x"17", x"18", x"19", x"1A", x"25", x"26", x"27", x"28",
+            x"29", x"2A", x"34", x"35", x"36", x"37", x"38", x"39",
+            x"3A", x"43", x"44", x"45", x"46", x"47", x"48", x"49",
+            x"4A", x"53", x"54", x"55", x"56", x"57", x"58", x"59",
+            x"5A", x"63", x"64", x"65", x"66", x"67", x"68", x"69",
+            x"6A", x"73", x"74", x"75", x"76", x"77", x"78", x"79",
+            x"7A", x"83", x"84", x"85", x"86", x"87", x"88", x"89",
+            x"8A", x"92", x"93", x"94", x"95", x"96", x"97", x"98",
+            x"99", x"9A", x"A2", x"A3", x"A4", x"A5", x"A6", x"A7",
+            x"A8", x"A9", x"AA", x"B2", x"B3", x"B4", x"B5", x"B6",
+            x"B7", x"B8", x"B9", x"BA", x"C2", x"C3", x"C4", x"C5",
+            x"C6", x"C7", x"C8", x"C9", x"CA", x"D2", x"D3", x"D4",
+            x"D5", x"D6", x"D7", x"D8", x"D9", x"DA", x"E1", x"E2",
+            x"E3", x"E4", x"E5", x"E6", x"E7", x"E8", x"E9", x"EA",
+            x"F1", x"F2", x"F3", x"F4", x"F5", x"F6", x"F7", x"F8",
+            x"F9", x"FA",
+        x"FF", x"C4", x"00", x"1F", x"01",--DHT C DC
+            x"00", x"03", x"01", x"01", x"01", x"01", x"01", x"01", x"01", x"01", x"01", x"00", x"00", x"00", x"00", x"00",
+            x"00", x"01", x"02", x"03", x"04", x"05", x"06", x"07", x"08", x"09", x"0A", x"0B",
+        x"FF", x"C4", x"00", x"B5", x"11",--DHT C AC
+            x"00", x"02", x"01", x"02", x"04", x"04", x"03", x"04", x"07", x"05", x"04", x"04", x"00", x"01", x"02", x"77",
+            x"00", x"01", x"02", x"03", x"11", x"04", x"05", x"21", 
+            x"31", x"06", x"12", x"41", x"51", x"07", x"61", x"71", 
+            x"13", x"22", x"32", x"81", x"08", x"14", x"42", x"91", 
+            x"A1", x"B1", x"C1", x"09", x"23", x"33", x"52", x"F0", 
+            x"15", x"62", x"72", x"D1", x"0A", x"16", x"24", x"34", 
+            x"E1", x"25", x"F1", x"17", x"18", x"19", x"1A", x"26", 
+            x"27", x"28", x"29", x"2A", x"35", x"36", x"37", x"38", 
+            x"39", x"3A", x"43", x"44", x"45", x"46", x"47", x"48", 
+            x"49", x"4A", x"53", x"54", x"55", x"56", x"57", x"58", 
+            x"59", x"5A", x"63", x"64", x"65", x"66", x"67", x"68", 
+            x"69", x"6A", x"73", x"74", x"75", x"76", x"77", x"78", 
+            x"79", x"7A", x"82", x"83", x"84", x"85", x"86", x"87", 
+            x"88", x"89", x"8A", x"92", x"93", x"94", x"95", x"96", 
+            x"97", x"98", x"99", x"9A", x"A2", x"A3", x"A4", x"A5", 
+            x"A6", x"A7", x"A8", x"A9", x"AA", x"B2", x"B3", x"B4", 
+            x"B5", x"B6", x"B7", x"B8", x"B9", x"BA", x"C2", x"C3", 
+            x"C4", x"C5", x"C6", x"C7", x"C8", x"C9", x"CA", x"D2", 
+            x"D3", x"D4", x"D5", x"D6", x"D7", x"D8", x"D9", x"DA", 
+            x"E2", x"E3", x"E4", x"E5", x"E6", x"E7", x"E8", x"E9",
+            x"EA", x"F2", x"F3", x"F4", x"F5", x"F6", x"F7", x"F8",
+            x"F9", x"FA",
+        x"FF", x"DA", x"00", x"0C", x"03", x"01", x"00", x"02", x"11", x"03", x"11", x"00", x"3F", x"00",--SOS
+        x"FF", x"D9" --EOI
+    );
+    
+    type state_type is (idle,header,waiting,data,eoi);
+    signal current_state, next_state : state_type;
+    signal ent_buf: std_logic_vector(7 downto 0);
+    signal array_el : integer range 0 to 1023;
+    signal entropy_el : integer range 0 to 7 := 7;
+
+    signal ent_load: std_logic_vector(1 downto 0);
+    signal valid: std_logic;
+    signal datareg: std_logic_vector(31 downto 0);
+    signal keep: std_logic_vector(3 downto 0);
+    
+begin
+
+    state_update: process(clk,rst)
+    begin
+        if rst='1' then
+            current_state <= idle;
+        elsif rising_edge(clk) then
+            current_state<=next_state;          
+        end if;
+    end process;
+    
+    state_change: process(current_state,start,array_el,datavalid,done)
+    begin
+        case current_state is
+            when idle =>
+                if start='1' then
+                    next_state <= header;
+                end if;
+            when header =>
+                if array_el=620 then
+                    next_state <= waiting;
+                end if;
+            when waiting =>  
+                if datavalid='1' then
+                    next_state <= data;
+                end if;
+            when data =>    
+                if done='1' then
+                    next_state <= eoi;
+                end if;
+            when eoi=>       
+                next_state <= idle;
+            when others => null;
+        end case;
+    end process;
+    
+    writing_proc: process(clk)
+    begin
+        if rising_edge(clk) then
+            if current_state = idle then
+                datareg <= (others =>'0');
+            elsif current_state = header then
+                valid<='1';
+                if array_el = 620 then
+                    datareg <= x"00" & img_info(array_el) & img_info(array_el+1) & img_info(array_el+2);
+                    keep <= "0111";
+                    array_el <= 0;
+                else
+                    if array_el=160 then    
+                        datareg <= img_info(array_el) & img_info(array_el+1) & img_info(array_el+2) & height(15 downto 8);
+                    elsif array_el=164 then
+                        datareg <= height(7 downto 0) & width(15 downto 8) & width(7 downto 0) & img_info(171);
+                    else
+                        datareg <= img_info(array_el) & img_info(array_el+1) & img_info(array_el+2) & img_info(array_el+3);
+                    end if;
+                    keep <= "1111";
+                    array_el <= array_el + 4;
+                end if;
+            elsif current_state = data then
+                if datavalid='1' then
+                    ent_buf(entropy_el) <= in_bit;
+                    if entropy_el = 0 then
+                        entropy_el <= 7;
+                        ent_load<="01";
+                        if ent_buf = "11111111" then
+                            ent_load<="11";
+                        end if;
+                    else
+                        entropy_el <= entropy_el - 1;
+                        if done='1' then
+                            for i in entropy_el downto 0 loop
+                                ent_buf(i) <= '1';
+                            end loop;
+                            ent_load<="01";
+                            if ent_buf = "11111111" then
+                                ent_load<="11";
+                            end if;
+                            valid <= '1';
+                        end if;  
+                    end if;
+                end if;
+                if ent_load="01" then
+                    datareg <= x"000000" & ent_buf;
+                    keep <= "0001";
+                    valid<='1';
+                    ent_load<="00";
+                elsif ent_load="11" then
+                    datareg <= x"0000" & ent_buf & x"00";
+                    keep <= "0011";
+                    valid<='1';
+                    ent_load<="00";
+                elsif axi_ready = '1' then
+                    valid <= '0';
+                end if;
+            elsif current_state = eoi then
+                datareg <= x"0000" & img_info(623) & img_info(624);
+                keep <= "0011";
+            else
+                valid<='0';
+                keep<="0000";
+            end if;
+        end if;
+    end process;
+    
+    dataready<='1' when current_state = waiting else '0';
+
+    axi_data <= datareg;
+    axi_valid <= valid;
+    axi_keep <= keep;
+    
+end Behavioral;
---
+++ connection/rtl/new/wrapper_fw_included.vhdl
@@ -0,0 +1,384 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+use ieee.std_logic_unsigned.all;
+use work.FixedPoint_Types.all;
+
+entity wrapper is 
+    port (
+        clk : in std_logic;
+        rst_n : in std_logic;
+        start : in std_logic;
+        R : in std_logic_vector(511 downto 0);
+        G : in std_logic_vector(511 downto 0);
+        B : in std_logic_vector(511 downto 0);
+        conversion_Y_out : out std_logic_vector(1023 downto 0); --Only for debugging
+        dct_Y_out : out std_logic_vector(1023 downto 0); --Only for debugging
+        quant_Y_out : out std_logic_vector(1023 downto 0); --Only for debugging
+        zigzag_Y_out : out std_logic_vector(1023 downto 0); --Only for debugging
+        stored_huffman : out std_logic_vector(1100 downto 0);
+        fw_data: out std_logic_vector(31 downto 0);
+        fw_keep: out std_logic_vector (3 downto 0))
+        finished : out std_logic
+    );
+end wrapper;
+
+architecture wrapper_arch of wrapper is
+    
+    component conversion is
+        generic (
+            scale : integer := 8;
+            fixed_point_length : integer := 16;
+            input_width : integer := 8
+        );
+        port (
+            clk : in std_logic;
+            start : in std_logic;
+            input_R : in std_logic_vector(input_width - 1 downto 0);
+            input_G : in std_logic_vector(input_width - 1 downto 0);
+            input_B : in std_logic_vector(input_width - 1 downto 0);
+            output_Y  : out std_logic_vector(fixed_point_length - 1 downto 0);
+            output_Cb : out std_logic_vector(fixed_point_length - 1 downto 0);
+            output_Cr : out std_logic_vector(fixed_point_length - 1 downto 0);
+            done : out std_logic
+        );
+    end component conversion;
+
+    component dct8x8_chen_2d is
+        generic(
+            IN_W : integer := 32;
+            FRAC : integer := 8
+        );
+        port (
+            clk : in std_logic;
+            rst_n : in std_logic;
+            in_valid : in std_logic;
+            in_ready : out std_logic;
+            in_data : in std_logic_vector(64*IN_W-1 downto 0);
+            out_valid : out std_logic;
+            out_ready : in std_logic;
+            out_data : out std_logic_vector(64*IN_W-1 downto 0)
+        );
+    end component dct8x8_chen_2d;
+
+    component quantization is 
+        port(
+            clk : in std_logic;
+            Y : in fixed_array;
+            Cb : in fixed_array;
+            Cr : in fixed_array;
+            Y_out : out fixed_array_16;
+            Cb_out : out fixed_array_16;
+            Cr_out : out fixed_array_16
+        );
+    end component quantization;
+
+    component zigzag_reorder is
+        generic (
+            vector_length : integer := 16
+        );
+        port (
+            clk : in std_logic;
+            input_matrix : in  std_logic_vector(64*vector_length-1 downto 0); -- 64 x vector_length flattened input
+            zigzag_out   : out std_logic_vector(64*vector_length-1 downto 0)  -- 64 x vector_length flattened output
+        );
+    end component zigzag_reorder;
+    
+    component huff_container is 
+        port(
+            clk : in std_logic;
+            reset : in std_logic;
+            start_huffman : in std_logic;
+            Y : in std_logic_vector(1023 downto 0);
+            Cb : in std_logic_vector(1023 downto 0);
+            Cr : in std_logic_vector(1023 downto 0);
+            data : out std_logic;
+            data_valid : out std_logic;
+            finished : out std_logic	
+        );
+    end component huff_container;
+
+    component fwrit_main is
+    Port ( height, width: in std_logic_vector (15 downto 0);
+           clk, rst, in_bit, start, datavalid, done: in std_logic;
+           dataready: out std_logic; --signals Huffman block that we are ready to receive data
+           axi_valid: out std_logic;
+           axi_ready: in std_logic;
+           axi_data: out std_logic_vector(31 downto 0);
+           axi_keep: out std_logic_vector (3 downto 0));
+    end component fwrit_main;
+
+    type t_State is (idle,RGBtoYCbCr,dct,quant_load,quant_read,zigzag,huff_load,huff_read,done);
+    signal currentState : t_State := idle;
+
+    type dct_states is (Y_state_send,Y_state_middle,Y_state_wait,Cb_state_send,Cb_state_wait,Cr_state_send,Cr_state_wait);
+    signal dct_state : dct_states := Y_state_send;
+
+    signal convertStart : std_logic := '0';
+    signal input_R : std_logic_vector(7 downto 0);
+    signal input_G : std_logic_vector(7 downto 0);
+    signal input_B : std_logic_vector(7 downto 0);
+    signal output_Y : std_logic_vector(15 downto 0);
+    signal output_Cb : std_logic_vector(15 downto 0);
+    signal output_Cr : std_logic_vector(15 downto 0);
+    signal convertDone : std_logic;
+
+    signal Y : std_logic_vector(1023 downto 0);
+    signal Cb : std_logic_vector(1023 downto 0);
+    signal Cr : std_logic_vector(1023 downto 0);
+    signal in_valid_dct : std_logic := '0';
+    signal in_ready_dct : std_logic;
+    signal out_valid_dct: std_logic;
+    signal out_ready_dct : std_logic := '0';
+    signal in_data_dct : std_logic_vector(1023 downto 0);
+    signal out_data_dct : std_logic_vector(1023 downto 0);
+
+    signal Y_array : fixed_array;
+    signal Cb_array : fixed_array;
+    signal Cr_array : fixed_array;
+    signal Y_array_out : fixed_array_16;
+    signal Cb_array_out : fixed_array_16;
+    signal Cr_array_out : fixed_array_16;
+
+    signal zigzag_in : std_logic_vector(1023 downto 0);
+    signal zigzag_out : std_logic_vector(1023 downto 0);
+
+    signal huff_data_out : std_logic;
+    signal huff_start : std_logic;
+    signal huff_data_valid : std_logic;
+    signal huff_finished : std_logic;
+    signal huff_Y : std_logic_vector(1023 downto 0);
+    signal huff_Cb : std_logic_vector(1023 downto 0);
+    signal huff_Cr : std_logic_vector(1023 downto 0);
+
+    begin
+
+        comp_conversion : component conversion
+            generic map(scale => 8, fixed_point_length => 16, input_width => 8 )
+            port map(
+                clk => clk,
+                start => convertStart,
+                input_R => input_R,
+                input_G => input_G,
+                input_B => input_B,
+                output_Y => output_Y,
+                output_Cb => output_Cb,
+                output_Cr => output_Cr,
+                done => convertDone
+            );
+
+        comp_dct : component dct8x8_chen_2d 
+            generic map(IN_W => 16, FRAC => 8)
+            port map(
+                clk => clk,
+                rst_n => rst_n,
+                in_valid => in_valid_dct,
+                in_ready => in_ready_dct,
+                in_data => in_data_dct,
+                out_valid => out_valid_dct,
+                out_ready => out_ready_dct,
+                out_data => out_data_dct
+            );
+
+        comp_quant : component quantization
+            port map(
+                clk => clk,
+                Y => Y_array,
+                Cb => Cb_array,
+                Cr => Cr_array,
+                Y_out => Y_array_out,
+                Cb_out => Cb_array_out,
+                Cr_out => Cr_array_out
+            );
+
+        comp_zigzag : component zigzag_reorder
+            generic map(vector_length => 16)
+            port map(
+                clk => clk,
+                input_matrix => zigzag_in,
+                zigzag_out => zigzag_out
+            );
+        
+        comp_huff : component huff_container
+            port map(
+                clk => clk,
+                reset => rst_n,
+                start_huffman => huff_start,
+                Y => huff_Y,
+                Cb => huff_Cb, 
+                Cr => huff_Cr,
+                data => huff_data_out,
+                data_valid => huff_data_valid,
+                finished => huff_finished
+            );
+
+        comp_file_writ: component fwrit_main
+            port map(
+                clk => clk,
+                rst => rst_n,
+                start => convertStart,
+                in_bit => huff_data_out,
+                datavalid => huff_data_valid,
+                done => huff_finished,
+                dataready => huff_start,
+                axi_valid => null,
+                axi_ready => null,
+                axi_data => fw_data,
+                axi_keep => fw_keep
+            )
+
+        proc : process(clk)
+            variable index : integer := 0;
+            variable state : integer := 0;
+        begin
+            if rising_edge(clk) then
+                case currentState is    
+                    when idle =>
+                        index := 0;
+                        dct_state <= Y_state_send;
+                        finished <= '0';
+                        state := 0;
+                        if start = '1' then
+                            currentState <= RGBtoYCbCr;
+                        end if;
+
+                    when RGBtoYCbCr => 
+                        if state = 0 then
+                            input_R <= R(8*(index+1)-1 downto 8*index);
+                            input_G <= G(8*(index+1)-1 downto 8*index);
+                            input_B <= B(8*(index+1)-1 downto 8*index);
+                            convertStart <= '1';
+                            state := 1;
+                        elsif state = 1 then
+                            convertStart <= '0';
+                            if convertDone = '1' then
+                                Y(16*(index+1)-1 downto 16*(index)) <= output_Y;
+                                Cb(16*(index+1)-1 downto 16*(index)) <= output_Cb;
+                                Cr(16*(index+1)-1 downto 16*(index)) <= output_Cr;     
+                                index := index + 1;
+                                state := 0;   
+                            end if;
+                            if index = 64 then
+                                index := 0;
+                                currentState <= dct;
+                                conversion_Y_out <= Y;
+                                --currentState <= done; --By chaning to go here directly this output is printed. 
+                            end if;
+                        end if;
+
+                    when dct => 
+                        case dct_state is
+                            when Y_state_send =>
+                                in_data_dct <= Y;
+                                in_valid_dct <= '1';         
+                                if in_ready_dct = '1' then       
+                                   dct_state <= Y_state_wait;
+                                end if;
+
+                            when Y_state_wait =>
+                                in_valid_dct <= '0';
+                                out_ready_dct <= '1';
+                                if out_valid_dct = '1' then
+                                    out_ready_dct <= '0';
+                                    Y <= out_data_dct;
+                                    dct_state <= Cb_state_send;
+                                end if;
+
+                            when Cb_state_send =>
+                                in_data_dct <= Cb;
+                                in_valid_dct <= '1';
+                                if in_ready_dct = '1' then       
+                                   dct_state <= Cb_state_wait;
+                                end if;
+
+                            when Cb_state_wait =>
+                                in_valid_dct <= '0';
+                                out_ready_dct <= '1';
+                                if out_valid_dct = '1' then
+                                    out_ready_dct <= '0';
+                                    Cb <= out_data_dct;
+                                    dct_state <= Cr_state_send;
+                                end if;
+
+                            when Cr_state_send =>
+                                in_data_dct <= Cr;
+                                in_valid_dct <= '1';
+                                if in_ready_dct = '1' then       
+                                   dct_state <= Cr_state_wait;
+                                end if;
+
+                            when Cr_state_wait =>
+                                in_valid_dct <= '0';
+                                out_ready_dct <= '1';
+                                if out_valid_dct = '1' then
+                                    out_ready_dct <= '0';
+                                    Cr <= out_data_dct;
+                                    index := 0;
+                                    --currentState <= done;
+                                    currentState <= quant_load;
+                                    dct_Y_out <= Y;
+                                end if;
+                                
+                            when others =>
+                                currentState <= idle;
+                                
+                        end case;
+
+                    when quant_load =>
+                        for i in 0 to 63 loop
+                            Y_array(index) <= "00000000" & Y(16*(index+1)-1 downto 16*index) & "00000000";
+                            Cb_array(index) <= "00000000" & Cb(16*(index+1)-1 downto 16*index) & "00000000";
+                            Cr_array(index) <= "00000000" & Cr(16*(index+1)-1 downto 16*index) & "00000000";
+                        end loop;
+                        currentState <= quant_read;
+                        
+                    when quant_read =>
+                        for i in 0 to 63 loop
+                            Y(16*(index+1)-1 downto 16*index) <= Y_array_out(index);
+                            quant_Y_out(16*(index+1)-1 downto 16*index) <= Y_array_out(index);
+                            Cb(16*(index+1)-1 downto 16*index) <= Cb_array_out(index);
+                            Cr(16*(index+1)-1 downto 16*index) <= Cr_array_out(index);
+                        end loop;
+                        currentState <= zigzag;
+
+                    when zigzag =>
+                        if state = 1 then
+                            zigzag_in <= Y;
+                        elsif state = 3 then
+                            Y <= zigzag_out;
+                            zigzag_in <= Cb;
+                        elsif state = 5 then
+                            Cb <= zigzag_out;
+                            zigzag_in <= Cr;
+                        elsif state = 7 then
+                            Cr <= zigzag_out;
+                            currentState <= huff_load;
+                        end if;
+                        state := state + 1;
+                    
+                    when huff_load =>
+                        zigzag_Y_out <= Y;
+                        huff_Y <= Y;                           
+                        huff_Cb <= Cb;
+                        huff_Cr <= Cr;
+                        index := 0;
+                        currentState <= huff_read;
+
+                    when huff_read =>
+                        if huff_finished = '1' then
+                            currentState <= done;
+                        end if;
+
+                        if huff_data_valid = '1' then
+                            stored_huffman(index) <= huff_data_out;
+                            index := index +1;
+                        end if;
+            
+                    when done =>
+                        finished <= '1';
+                        
+                    when others => 
+                end case;
+            end if;
+        end process proc;
+end architecture;
